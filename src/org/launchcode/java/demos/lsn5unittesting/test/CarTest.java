67723229d1c4c35e43bcc434644e85e5bea6da76
Victoria <veerod0804@yahoo.com>
2022-09-12 11:17:26 -0500

    completed Unit Testing Exercises

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,11 +1,63 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
 public class CarTest {
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
-    //TODO: constructor sets gasTankLevel properly
-    //TODO: gasTankLevel is accurate after driving within tank range
-    //TODO: gasTankLevel is accurate after attempting to drive past tank range
-    //TODO: can't have more gas than tank size, expect an exception
+    Car test_car;
 
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
+
+    //TODO: constructor sets gasTankLevel properly
+
+    @Test
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+    }
+
+//    @Test
+//    public void testInitialGasTank() {
+//        assertFalse("Gas tank is empty", false);
+//    }
+
+//    @Test
+//    public void testInitialGasTank() {
+//        assertTrue("Gas tank is not empty", true);
+//    }
+
+    //TODO: gasTankLevel is accurate after driving within tank range
+
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(),.001);
+    }
+    //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+    //    drive takes in mpg (50) * gas tank level (10) = 500 (we want to go over 500)
+        test_car.drive(501);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+    //TODO: can't have more gas than tank size, expect an exception
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+     test_car.addGas(5);
+     fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
 }
